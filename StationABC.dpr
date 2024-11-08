1fd2603e5309a2060be2bd0159d2f89923b9e628
admin <admin@RAGNAR-PC.local>
2024-11-07 21:19:50 -0300

    Initial Commit

---
+++ StationABC.dpr
@@ -0,0 +1,14 @@
+program StationABC;
+
+uses
+  Vcl.Forms,
+  UMain in 'src\main\UMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
