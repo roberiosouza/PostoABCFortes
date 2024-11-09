92b4216ffdbf7440b1bf45876ab35259f22e33b5
roberio <rroberiosouza@gmail.com>
2024-11-09 15:04:17 -0300

    Adicionado o frame base dos cadastros

---
+++ src/main/frames/FRRegistrations.pas
@@ -0,0 +1,25 @@
+unit FRRegistrations;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Buttons, Vcl.StdCtrls;
+
+type
+  TFRCRUD = class(TFrame)
+    Label1: TLabel;
+    edtNameDescription: TEdit;
+    btnSave: TBitBtn;
+    btnCancel: TBitBtn;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.dfm}
+
+end.
