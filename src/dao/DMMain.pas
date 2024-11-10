87920efa8eab4c2823e2c6bdf1f7ad5eb63b84c5
roberio <rroberiosouza@gmail.com>
2024-11-09 21:24:09 -0300

    Adicionado no datamodule a referencia dos objetos da bomba e configurações

--- src/dao/DMMain.pas
+++ src/dao/DMMain.pas
@@ -7,7 +7,7 @@ uses
   FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
   FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.FB,
   FireDAC.Phys.FBDef, FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client, CEmployee,
-  CTank;
+  CTank, CPump, CConfig;
 
 type
   TDM = class(TDataModule)
@@ -19,6 +19,8 @@ type
     { Public declarations }
     Employee : Employee;
     Tank : Tank;
+    Pump: Pump;
+    Config: Config;
   end;
 
 var
