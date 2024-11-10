f327ec5a48d9504b255e148a6d7a1151d457ab8e
roberio <rroberiosouza@gmail.com>
2024-11-10 08:37:03 -0300

    Implementado o método ListAll na classe de funcionários

--- src/class/CEmployee.pas
+++ src/class/CEmployee.pas
@@ -17,7 +17,8 @@ Type
       FStatus: Char;
 
     public
-      constructor Create(Connection : TFDConnection; Transction: TFDTransaction);
+      constructor Create(Connection : TFDConnection; Transction: TFDTransaction); overload;
+      constructor Create; overload;
       destructor Destroy; Override;
 
       property Conection: TFDConnection read FConnection write FConnection;
@@ -28,6 +29,7 @@ Type
 
       procedure inactive(id: Integer);
       function save(employee: Employee): Boolean;
+      function ListAll(): TArray<Employee>;
   end;
 
 implementation
@@ -35,17 +37,29 @@ implementation
 uses
   Vcl.Dialogs, System.SysUtils;
 
+var
+  QueryListAll: TFDQuery;
+
 { Employee }
 
 constructor Employee.Create(Connection: TFDConnection; Transction: TFDTransaction);
 begin
   FConnection := Connection;
   FTransaction := Transaction;
+
+  QueryListAll := TFDQuery.Create(nil);
+  QueryListAll.Connection := FConnection;
+end;
+
+constructor Employee.Create;
+begin
+  inherited Create;
 end;
 
 destructor Employee.Destroy;
 begin
 
+  QueryListAll.Destroy;
   inherited;
 end;
 
@@ -68,6 +82,45 @@ begin
   end;
 end;
 
+function Employee.ListAll: TArray<Employee>;
+var
+  List: TArray<Employee>;
+  VEmployee: Employee;
+  Cont: Integer;
+begin
+  try
+    try
+      FConnection.Connected := False;
+      FConnection.Connected := True;
+
+      QueryListAll.close;
+      QueryListAll.SQL.Clear;
+      QueryListAll.SQL.Add('SELECT ID, NOME FROM TBFUNCIONARIO WHERE STATUS = '+QuotedStr('S')+' ORDER BY NOME ASC');
+      QueryListAll.Open;
+
+      SetLength(List, QueryListAll.RowsAffected);
+
+      Cont := 0;
+      QueryListAll.First;
+      while not QueryListAll.eof do
+      begin
+        VEmployee := Employee.Create;
+        VEmployee.Id := QueryListAll.FieldByName('ID').AsInteger;
+        VEmployee.Nome := QueryListAll.FieldByName('NOME').AsString;
+
+        List[cont] := VEmployee;
+
+        Cont := Cont + 1;
+        QueryListAll.Next;
+      end;
+    finally
+      Result := List;
+    end;
+  except on E: Exception do
+     Showmessage('Ocorreu um erro: ' + e.Message);
+  end;
+end;
+
 function Employee.Save(employee: Employee): Boolean;
 begin
   try
@@ -84,7 +137,7 @@ begin
     begin
       FConnection.Rollback;
       Result := False;
-      showmessage('Ocorreu um erro: ' + e.Message);
+      Showmessage('Ocorreu um erro: ' + e.Message);
     end;
   end;
 end;
